24e92f1d03b4c0b67f30c163c1c3d3a4d80f6c54
Teodor Szente <teodor98sz@gmail.com>
2020-04-13 10:58:30 +0300

    Solve B

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/MainActivity.java
@@ -2,13 +2,66 @@ package ro.pub.cs.systems.eim.Colocviu1_13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
+
+    String SOUTH = "SOUTH";
+    String NORTH = "NORTH";
+    String WEST = "WEST";
+    String EAST = "EAST";
+
+    ArrayList<String> directions =  new ArrayList<>();
+    Integer noDirections = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences mPrefs = getSharedPreferences("dirs", MODE_PRIVATE);
+        noDirections = mPrefs.getInt("noDirections", 0);
+
+
     }
+    @Override
+    protected  void onPause() {
+        super.onPause();
+        SharedPreferences.Editor ed = getSharedPreferences("dirs", MODE_PRIVATE).edit();
+        ed.putInt("noDirections", directions.size());
+        ed.commit();
+    }
+
+
+
+    public void updateText() {
+        TextView text = findViewById(R.id.textView);
+        text.setText(directions.stream()
+                .collect(Collectors.joining(", ")));
+
+    }
+
+    public void handleNorth(View view) {
+        directions.add(NORTH);
+        updateText();
+    }
+
+    public void handleSouth(View view) {
+        directions.add(SOUTH);
+        updateText();
+    }
+    public void handleWest(View view) {
+        directions.add(WEST);
+        updateText();
+    }
+    public void handleEast(View view) {
+        directions.add(EAST);
+        updateText();
+    }
+
 }
